e6ec5f4b76f29a80292c7e74eccb9f76a0b86513
Anastasia Zizeko <nastjaznf@gmail.com>
2020-03-05 23:52:01 +0300

    feat: solve expression-calculator task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let stack = [];
+    stack = expr.split('');
+    let countOfLeftBrackets = 0,
+        countOfRightBrackets = 0;
+    stack.forEach(element => {
+        if (element === '(') countOfLeftBrackets++;
+        if (element === ')') countOfRightBrackets++;
+    });
+
+    if (countOfLeftBrackets !== countOfRightBrackets) throw new Error('ExpressionError: Brackets must be paired');
+    if (expr.includes('/ 0')) { throw new TypeError('TypeError: Division by zero.') }
+
+    return new Function('return ' + expr)();
+
 }
 
 module.exports = {
