d1919b4f68615c41e4b2384a81d65a980e777200
Anastasia Zizeko <nastjaznf@gmail.com>
2020-03-07 20:39:55 +0300

    fix: fix last solution; feat: add new solution

--- src/index.js
+++ src/index.js
@@ -3,20 +3,81 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    let stack = [];
-    stack = expr.split('');
-    let countOfLeftBrackets = 0,
-        countOfRightBrackets = 0;
-    stack.forEach(element => {
-        if (element === '(') countOfLeftBrackets++;
-        if (element === ')') countOfRightBrackets++;
+function add(string) {
+
+    let array = string.split('+');
+    return array.map(sub).reduce((x, y) => { return x + y });
+}
+
+function sub(string) {
+
+    let array = string.split('-');
+    return array.map(multiply).reduce((x, y) => { return x - y });
+}
+
+function multiply(string) {
+
+    let array = string.split('*');
+    return array.map(divide).reduce((x, y) => { return x * y });
+}
+
+function divide(string) {
+
+    let array = string.split('/');
+    return array.map((num) => {
+        num = num.replace('tempMinus', '-')
+        return Number(num);
+    }).reduce((x, y) => {
+        if (y === 0) {
+            throw 'TypeError: Division by zero.';
+        } else {
+            return x / y;
+        }
     });
+}
 
-    if (countOfLeftBrackets !== countOfRightBrackets) throw new Error('ExpressionError: Brackets must be paired');
-    if (expr.includes('/ 0')) { throw new TypeError('TypeError: Division by zero.') }
 
-    return new Function('return ' + expr)();
+
+function getExprFromBrackets(string) {
+
+    let leftBracket = string.lastIndexOf('(');
+    let rightBracket = string.indexOf(')', leftBracket);
+
+    if ((leftBracket === -1) || (rightBracket === -1)) {
+        throw 'ExpressionError: Brackets must be paired';
+    }
+
+    let currentBracket = add(string.substring(leftBracket + 1, rightBracket));
+    currentBracket = (currentBracket.toString()).replace('-', 'tempMinus');
+
+    let result = string.substring(0, leftBracket) + currentBracket + string.substring(rightBracket + 1);
+    return result;
+}
+
+function expressionCalculator(expr) {
+    /* More algorithmic variant: */
+    let exprString = expr.replace(/\s+/g, '');
+
+    while ((exprString.indexOf('(') != -1) || (exprString.indexOf(')') != -1)) {
+        exprString = getExprFromBrackets(exprString);
+    }
+
+    return (add(exprString));
+
+    /*  Another working variant:
+     let stack = [];
+     stack = expr.split('');
+     let countOfLeftBrackets = 0,
+         countOfRightBrackets = 0;
+     stack.forEach(element => {
+         if (element === '(') countOfLeftBrackets++;
+         if (element === ')') countOfRightBrackets++;
+     });
+
+     if (countOfLeftBrackets !== countOfRightBrackets) throw new Error('ExpressionError: Brackets must be paired');
+     if (expr.includes('/ 0')) { throw new TypeError('TypeError: Division by zero.') }
+
+     return window('return ' + expr)(); */
 
 }
 
